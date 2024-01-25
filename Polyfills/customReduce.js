74cfa7c1146f6a59e0372301fa4ddaf8a6d6d413
Mukul Kumar <mkunboxing@gmail.com>
2024-01-25 18:47:13 +0530

    added

--- Polyfills/customReduce.js
+++ Polyfills/customReduce.js
@@ -1,1 +1,25 @@
-wegwg wet 
+// Pollyfill for Array.reduce()
+// Let’s first see how Array.reduce() works from its syntax:
+
+arr.reduce(callback( accumulator, currentValue, [, index[, array]] )[, initialValue])
+
+/*So, the original Array.reduce function takes two arguement :
+1. A callback function as an argument and that callback function can have four arguments passed into it :
+a. accumulator
+b. current value
+c. index of the current value [optional]
+d. array [optional] */
+
+// Based on that, let’s build our own reduce function :
+
+Array.prototype.myReduce= function(callbackFn, initialValue) {
+    var accumulator = initialValue;
+  for (var i = 0; i < this.length; i++) {
+      if (accumulator !== undefined) {
+        accumulator = callbackFn.call(undefined, accumulator, this[i],   i, this);
+      } else {
+        accumulator = this[i];
+      }
+    }
+    return accumulator;
+  }
