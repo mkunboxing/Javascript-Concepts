c078f6cc1b743d2cc5b372612474b66dee97a556
Mukul Kumar <mkunboxing@gmail.com>
2024-01-26 20:39:18 +0530

    added

---
+++ Polyfills/promiseBind.js
@@ -0,0 +1,15 @@
+// The basic implementation of bind goes like
+
+let basic = {
+    'name': 'shyam',
+    'age': 24
+  };
+  
+  function callMe(city) {
+    console.log('Hi! my name is ' + this.name + ' and my age is ' + this.age + ' and my city is ' + arguments[0] + ' and state is ' + arguments[1]);
+  }
+  let callBinded = callMe.bind(basic, 'jammu');
+  callBinded('j&k'):
+
+//   Output: Hi! my name is shyam and my age is 24 and my city is jammu and state is j&k
+
