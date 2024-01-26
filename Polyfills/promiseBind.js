330e91ce4eece9f8b570fac278fb988c4ccc9f51
Mukul Kumar <mkunboxing@gmail.com>
2024-01-26 20:52:43 +0530

    added

--- Polyfills/promiseBind.js
+++ Polyfills/promiseBind.js
@@ -13,3 +13,20 @@ let basic = {
 
 //   Output: Hi! my name is shyam and my age is 24 and my city is jammu and state is j&k
 
+
+////////////////////////////////////////////////////////////////////////////////////
+
+
+// Polyfill for bind():
+
+Function.prototype.myBind = function(...args){
+    var callback = this,
+        ctx = args.splice(1);
+    return function(...a){        
+        callback.call(args[0], ...[...ctx, ...a]);
+    }
+}
+
+const result2 = printName.myBind(myName, "Palia",);
+result2("India");
+
